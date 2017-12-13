83abc01c999a8bede4b222128439bd8f65f1105b
evthriat <evantatkinson@gmail.com>
2017-12-13 11:49:29 -0800

    refactored twice, working for dynamically added

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 // TODO
-var GrocceryList = () => {
-	<div>
-		<li>
-		<ul>Kittens</ul>
-		<ul>Pumpkins</ul>
-		</li>
-	</div>
+var GrocceryList = () => (
+		<span>
+		  <GrocceryListItems items = {['this', 'that', 'the other']}/>
+		</span>
+)
 
-}
+var Kittens = () => (
+  <ul>kittens</ul>
+)
+var Pumpkins = () => (
+  <ul>pumpkin</ul>
+)
+
+var GrocceryListItems = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+    <li>{props.items[2]}</li>
+  </ul>
+
+);
+
+
+ReactDOM.render(<GrocceryList />, document.getElementById("app"));
